44f91dc277c57325e6d42cdbae16ee2e2a273987
pratheek-rai14 <22mc041@sitmng.ac.in>
2024-10-23 18:59:26 +0000

    variable concept done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = "1401"
+let accountEmail = "pratheekrai@gmail.com"
+var accountPassword = "11223344"
+accountCity = "Bengaluru"
+let accountState;
+
+// accountId = "2" // Not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "09987"
+accountCity = "Mangaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
