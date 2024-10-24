4bf524bda4912a63df12f8b3db8776be8009221f
pratheek-rai14 <22mc041@sitmng.ac.in>
2024-10-24 19:52:44 +0000

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 < 2);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 >= 1);
+
+console.log("2" == 2);
+console.log("02" < 3);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(0 == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+// Avoid these type of code 
+
+// ===
+
+console.log("2" === 2);
+
+
+
+
+
+
+
