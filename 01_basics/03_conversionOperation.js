2522fd116f1e66975c6463e44894ac58a53b988f
pratheek-rai14 <22mc041@sitmng.ac.in>
2024-10-24 19:18:10 +0000

    ConversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,6 +25,58 @@ let isLoggedIn = 1
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************************ Operations *****************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3**3);
+// console.log(3/3);
+
+let str1 = "Hello"
+let str2 = " Pratheek"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(2 + "1");
+// console.log(2 + 3 + "1")
+// console.log("1" + 3 + 2);
+
+// console.log((3 + 4) * (5 / 3));
+
+// console.log(+true);   //Don't do like this
+// console.log(+"");    //Don't do like this
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2  //incorrect way to write code 
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+// link to Study
+// https://262.ecma-international.org/14.0/index.html?_gl=1*1eh4huy*_ga*MTU1NjQ2MDg5Ny4xNzI5Nzk1NjEy*_ga_TDCK4DWEPP*MTcyOTc5NTYxMi4xLjEuMTcyOTc5NjQxNy4wLjAuMA..#sec-type-conversion
+
+
+
+
+
+
+
+
+
+
+
+
+
 
