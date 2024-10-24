10a81fab34cdb1f82401909949670fb7777b1ab4
pratheek-rai14 <22mc041@sitmng.ac.in>
2024-10-24 08:07:02 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,35 @@
+"use scrict"; // treat all JS code as ac newer version
+
+// alert(3+3)  //we are using nodejs , not browser 
+
+console.log(3
+    +
+    3); //code readability should be high 
+
+console.log("Pratheek");
+
+let name = "Pratheek"
+let age = "20"
+let isLoggedIn = true
+let state;
+
+// premitive datatypes
+
+// number => 2 to power 53
+// bigint 
+// string = ""
+// boolean => true/false
+// null => Standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof 20);
+console.log(typeof null); //object 
+console.log(typeof undefined); //undefined
+
+
+
+
