5b2ad4c5ce644905ae34d15e595fec8926bcf4f5
pratheek-rai14 <22mc041@sitmng.ac.in>
2024-11-02 15:46:51 +0000

    Dates in javascript

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,12 +1,12 @@
 // Date 
 
 let myDate = new Date()    //Creating a date object
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toISOString());
-// console.log(myDate.toJSON());
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toLocaleString());
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
 
 // console.log(typeof myDate);
 
