33aa63d933ce15f01fa7465c3ab408c5cbf17d42
pratheek-rai14 <22mc041@sitmng.ac.in>
2024-11-13 19:30:38 +0000

    Scopes in JS

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,12 +4,49 @@ if (true) {
     const b = 20
     var c = 30
 
-    console.log("Inner:", (a));
+    // console.log("Inner:", (a));
     
 }
 
-console.log("Outer:", (a));
+// console.log("Outer:", (a));
 // console.log(b);
 // console.log(c);
 
 
+function one() {
+    const username = "Pratheek"
+
+        function two() {
+            const website = "Youtube"
+            console.log(username);
+        }
+        // console.log(website);
+        two()
+}
+// one()
+
+
+if (true) {
+    const username = "Pratheek"
+    
+    if (username === "Pratheek") {
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+}
+// console.log(username);  //Outside the function 
+
+
+
+// *********************** Interesting Example *****************************
+
+console.log(addone(5))
+
+function addone(num) {
+    return num + 1
+}
+
+addtwo(5)                           //It cannot access before the initialization of the variable
+const addtwo = function(num){
+    return num + 2
+}
